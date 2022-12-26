ccde221d297f27544c9bc2ac825fef51c4778486
Julio <IceXPR@users.noreply.github.com>
2022-12-26 00:11:58 -0500

    adding index.html

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
-from flask import Flask #importing the module
+from flask import Flask, current_app #importing the module
+
 
 app=Flask(__name__,
         static_url_path='', 
         static_folder='public') #instantiating flask object
 
 @app.route('/api/v1.0/garage') #defining a route in the application
-def func(): #writing a function to be executed 
+def garage(): #writing a function to be executed 
     return 'PythonGeeks'
+
+@app.route('/')
+def index():
+    return current_app.send_static_file('index.html')
 
 if __name__=='__main__': #calling  main 
     app.debug=True #setting the debugging option for the application instance
